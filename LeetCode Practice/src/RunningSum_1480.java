8977726ad6c4e9087fd69f499d61907f4a790039
sampadatalwar <sampadatalwar@gmail.com>
2020-07-20 21:10:44 +0530

    Leetcode Practice Problems

---
+++ LeetCode Practice/src/RunningSum_1480.java
@@ -0,0 +1,19 @@
+
+public class RunningSum_1480 {
+	
+	public int[] runningSum(int[] nums) {
+        
+        int sum = 0;
+        
+        for(int  i=0; i<nums.length; i++){
+            
+            sum += nums[i];
+            nums[i] = sum;
+            
+        }
+        
+        return nums;
+        
+    }
+
+}

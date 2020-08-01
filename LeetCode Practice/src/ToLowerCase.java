fa43870ff499189c8ce8074fa887796443adb18b
sampadatalwar <sampadatalwar@gmail.com>
2020-08-01 23:26:28 +0530

    written logic for lower case

--- LeetCode Practice/src/ToLowerCase.java
+++ LeetCode Practice/src/ToLowerCase.java
@@ -2,9 +2,33 @@
 public class ToLowerCase {
 	
 	public static void main(String[] args) {
-		String str = "Abc";
+		String str = "AbC";
 		str = str.toLowerCase();
-		System.out.println(str);
+		String str2 = converToLowerCase(str);
+		System.out.println(str+" "+str2);
+	}
+	
+	static String converToLowerCase(String str) {
+		
+		String s = "";
+        
+        for(int i=0; i<str.length(); i++){
+            
+            int j = str.charAt(i);
+            
+            
+            if (j >= 65 && j<=90){
+                char c = (char)(j+32);
+                s= s+String.valueOf(c);
+            }
+            else{
+                s = s+str.charAt(i);
+            }
+                
+            
+        }
+        
+        return s;
 	}
 
 }

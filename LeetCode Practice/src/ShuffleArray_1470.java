d96b91373dc0c53993be935a81d810dc978d077d
sampadatalwar <sampadatalwar@gmail.com>
2020-08-02 23:39:31 +0530

    Shuffle array problem

---
+++ LeetCode Practice/src/ShuffleArray_1470.java
@@ -0,0 +1,33 @@
+
+public class ShuffleArray_1470 {
+	
+	public static void main(String[] args) {
+		
+		int [] arr = {2,5,1,3,4,7};
+		
+		arr = shuffle(arr,3);
+		
+		System.out.print("Shuffled array: [" );
+		
+		for(int i=0; i<arr.length-1; i++) {
+			System.out.print(arr[i]+ ",");
+		}
+		System.out.println(arr[arr.length-1]+"]");
+	}
+	
+	public static int[] shuffle(int[] nums, int n) {
+    
+		int[] num = new int[nums.length];
+		int j = 0;
+    
+		for(int i =0; i<n; i++){
+       
+			num[j] = nums[i];
+			num[j+1] = nums[i+n];
+			j = j+2;
+		}
+		
+		return num;
+	}
+
+}

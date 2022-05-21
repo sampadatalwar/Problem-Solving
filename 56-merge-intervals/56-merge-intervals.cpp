1946eaf19fda4083b3ec79ba601645edd43f48de
Sampada Talwar <30980881+sampadatalwar@users.noreply.github.com>
2022-05-21 02:52:59 -0400

    Time: 39 ms (85.43%), Space: 19.1 MB (46.12%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        vector<vector<int>> result;
+
+        
+        if(intervals.size() == 1 || intervals.size() == 0)
+            return intervals;
+        
+        
+        bool check = false;
+        
+        sort(intervals.begin(), intervals.end());
+        //result.push_back(intervals[0]);
+        
+        vector<int> current = intervals[0];
+        
+        for(int i=1; i<intervals.size(); i++){
+            
+            vector<int> temp;
+             
+            
+            if(intervals[i][0] <= current[1]){
+                
+                //overlap exists
+                current[1] = max(intervals[i][1], current[1]);
+                
+            }else{
+                result.push_back(current);
+                current = intervals[i];
+            }
+        }
+           
+            
+        
+            result.push_back(current);
+            
+        
+        
+        return result;
+        
+    }
+};
